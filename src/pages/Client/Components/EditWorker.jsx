1d2a6882ac2e6da5636af0edea3f6033fd1b408b
pedro <pedrolucaspflores@gmail.com>
2023-09-27 14:12:41 -0300

    EditWorker changes

--- src/pages/Client/Components/EditWorker.jsx
+++ src/pages/Client/Components/EditWorker.jsx
@@ -4,10 +4,39 @@ import Btn from '../../components/ButtonComponent';
 import UseForm from '../../../Hooks/UseForm';
 import styles from '../CSS/Funcionarios.module.css';
 import { toast } from 'react-toastify';
+import { GET_SERVICES } from '../../../api';
 
 const EditWorker = ({ selectedWorkerId, workers, getWorkers }) => {
   let worker = workers.find((worker) => worker.id === selectedWorkerId);
   const [services, setServices] = React.useState([]);
+  const [allServices, setAllServices] = React.useState([]);
+  const [filteredServices, setFilteredServices] = React.useState([]);
+
+
+  // Token
+  const localUserString = window.localStorage.getItem('tccuser');
+  const localUser = JSON.parse(localUserString);
+  const token = localUser.token;
+
+  // Definindo todos os serviços deste cliente
+  async function getAllServices(){
+    const { url, options } = GET_SERVICES(token);
+
+    const response = await fetch(url, options);
+    const jsonRes = await response.json();
+    setAllServices(jsonRes)
+  }
+
+  React.useState(()=> {
+    getAllServices();
+    if (allServices && services) {
+      // Filtrar os serviços que não estão em "services"
+      const servicesNotInList = allServices.filter((service) => !services.includes(service.serviceID));
+      setFilteredServices(servicesNotInList);
+    } else {
+      setFilteredServices(allServices);
+    }
+  },[allServices, services])
 
   // Use o UseForm com o valor inicial
   let workerName = UseForm('');
@@ -24,9 +53,6 @@ const EditWorker = ({ selectedWorkerId, workers, getWorkers }) => {
       `Tem certeza que deseja excluir o funcionário ${workerName.value} ?`,
     );
     if (confirm) {
-      const localUserString = window.localStorage.getItem('tccuser');
-      const localUser = JSON.parse(localUserString);
-      const token = localUser.token;
 
       // const { url, options } = UPDATE_SERVICE(token, {
       //   id: worker.id,
@@ -62,18 +88,38 @@ const EditWorker = ({ selectedWorkerId, workers, getWorkers }) => {
     }
   }
 
+  function verificar() {
+    console.log("Services")
+    console.log(services)
+    console.log("All services:")
+    console.log(allServices)
+    console.log(services)
+    console.log("filteredServices:")
+    console.log(filteredServices)
+  }
+
   return (
+    <div>
+      
     <form
       onSubmit={handleEditWorker}
       autocomplete="off"
       className={styles.editWorkerForm}
     >
       <Input label="Nome" type="text" name="name" {...workerName} />
-      {services
-        ? services.map((service) => <p key={service.id}>{service.id}</p>)
-        : 'Sem serviços'}
+      <div>
+      {allServices
+        ? allServices.map((service) => 
+        <div key={service.id} className={services.includes(service.id) ? `${styles.disabledAddService}` : ''}>
+          <p>{service.name}</p>
+            <button onClick={verificar}>Adicionar</button>
+          </div>
+        )
+        : 'Sem serviços para adicionar'}
+        </div>
       <Btn>Atualizar</Btn>
     </form>
+    </div>
   );
 };
 
