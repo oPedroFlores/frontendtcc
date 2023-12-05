5220b060a94cb0c805c1e81f9edd720bbf41a17a
Pedro Flores <100242638+oPedroFlores@users.noreply.github.com>
2023-12-05 13:31:30 -0300

    fim

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,44 @@
-import React from 'react'
+import React from 'react';
 import NavBar from './components/NavBar';
-import styles from './CSS/Home.module.css'
-import cards from '../JSON/CardsHome.json'
+import styles from './CSS/Home.module.css';
+import cards from '../JSON/CardsHome.json';
 import { HomeCard } from './components/HomeCard';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-
   return (
     <section className={styles.home}>
-      <NavBar/>
+      <NavBar />
       <div className={styles.homeWrapper}>
         <div className={styles.homeSobre}>
           <div>
-            <p>A ONTime é responsável por realizar agendamentos para empresas, microeemprendedores e prestadores de serviços.</p>
-            <p>Aqui você encontrará um preço acessível, um ótimo atendimento e um serviço de ponta para o seu empreendimento!</p>
+            <p>
+              A ONTime é responsável por realizar agendamentos para empresas,
+              microeemprendedores e prestadores de serviços.
+            </p>
+            <p>
+              Aqui você encontrará um preço acessível, um ótimo atendimento e um
+              serviço de ponta para o seu empreendimento!
+            </p>
           </div>
-          <p>Entre em contato conosco por <Link to='/contato'>aqui</Link></p>
-          
-
-     </div>
+        </div>
         <div className={styles.homePlanos}>
           <div className={styles.homeCards}>
-          {cards.map((card, index) => (
-            <div key={index}>
-              <HomeCard card={card} index={index} />
-            </div>
-          ))}
+            {cards.map((card, index) => (
+              <div key={index}>
+                <HomeCard card={card} index={index} />
+              </div>
+            ))}
           </div>
           <div className={styles.homePlanosSobre}>
-            A ONTime possui três categorias de assinaturas, uma delas sendo até mesmo grátis! Verifique agora mesmo <Link to="/planos">clicando aqui!</Link>
-             </div>
+            A ONTime possui três categorias de assinaturas, uma delas sendo até
+            mesmo grátis! Verifique agora mesmo{' '}
+            <Link to="/planos">clicando aqui!</Link>
+          </div>
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
